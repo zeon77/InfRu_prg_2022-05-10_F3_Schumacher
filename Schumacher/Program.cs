ee92043cc17051a74055eca46b13dd3cc0db4207
zeon77 <zeon77@gmail.com>
2022-05-20 17:11:13 +0200

    5.

--- Schumacher/Program.cs
+++ Schumacher/Program.cs
@@ -22,6 +22,21 @@
                 .ToList()
                 .ForEach(x => Console.WriteLine($"\t{x.Dátum.ToString("yyyy. MM. dd.")}:{x.Pozíció}. hely"));
 
+            //5.
+            /*
+             * A feladat megfogalmazása szerint sikeres célba érés esetén a Státusz == "Finished".
+             * Ezek szerint a sikertelen célbaérés: Státusz != "Finished"
+             * Így viszont nem azonos az eredmény a mintával...
+             */
+            Console.WriteLine($"5. feladat: Hibastatisztika");
+            nagydíjak
+                .Where(x => x.Státusz != "Finished")
+                .GroupBy(x => x.Státusz)
+                .Select(g => new { Státusz = g.Key, db = g.Count() })
+                .Where(x => x.db > 2)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"\t{x.Státusz}: {x.db}"));
+
         }
     }
 }
