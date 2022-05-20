839c75e414eda13adfaa6dcc1b47a6313a6b9e27
zeon77 <zeon77@gmail.com>
2022-05-20 16:54:22 +0200

    2-3.

---
+++ Schumacher/Nagydíj.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schumacher
+{
+    internal class Nagydíj
+    {
+        public DateTime Dátum { get; set; }
+        public string Név { get; set; }
+        public int Pozíció { get; set; }
+        public int KörökSzáma { get; set; }
+        public int SzerzettPont { get; set; }
+        public string Csapat { get; set; }
+        public string Státusz { get; set; }
+
+        public Nagydíj(string sor)
+        {
+            string[] t = sor.Split(';');
+            Dátum = DateTime.Parse(t[0]);
+            Név = t[1];
+            Pozíció = int.Parse(t[2]);
+            KörökSzáma = int.Parse(t[3]);
+            SzerzettPont = int.Parse(t[4]);
+            Csapat = t[5];
+            Státusz = t[6];
+        }
+    }
+}
--- Schumacher/Program.cs
+++ Schumacher/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //2.
+            List<Nagydíj> nagydíjak = new List<Nagydíj>();
+            foreach (var sor in File.ReadAllLines("schumacher.csv").Skip(1))
+            {
+                nagydíjak.Add(new Nagydíj(sor));
+            }
+
+            //3.
+            Console.WriteLine($"3. feladat: {nagydíjak.Count}");
         }
     }
 }
