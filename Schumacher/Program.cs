e9d910ed18d243cb3d10d6d9c91179d3b0a3ba25
zeon77 <zeon77@gmail.com>
2022-05-20 17:06:25 +0200

    4.x

--- Schumacher/Program.cs
+++ Schumacher/Program.cs
@@ -15,11 +15,13 @@
             Console.WriteLine($"3. feladat: {nagydíjak.Count}");
 
             //4.
+            Console.WriteLine($"4. feladat: Magyar Nagydíj helyezései");
             nagydíjak
                 .Where(x => x.Név == "Hungarian Grand Prix" && x.Pozíció > 0)
                 //.Where(x => x.Név == "Hungarian Grand Prix" && x.Státusz == "Finished")
                 .ToList()
                 .ForEach(x => Console.WriteLine($"\t{x.Dátum.ToString("yyyy. MM. dd.")}:{x.Pozíció}. hely"));
+
         }
     }
 }
